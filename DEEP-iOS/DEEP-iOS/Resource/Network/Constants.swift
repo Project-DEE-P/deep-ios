61eafa1d4d8242b43d6ef817e2620204e5a1cced
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2023-10-24 12:27:56 +0900

    Add: Constants 추가 및 파일 위치 변경

---
+++ DEEP-iOS/DEEP-iOS/Resource/Network/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  DEEP-iOS
+//
+//  Created by 이민규 on 10/24/23.
+//
+
+import Foundation
+
+class APIConstants {
+    
+    static let api: String = "http://10.80.162.109:8081"
+//    static let api: String = "http://10.80.162.109:8082"
+    
+    /// auth
+    static let auth_register = "\(api)/v1/api/user/login"
+}
